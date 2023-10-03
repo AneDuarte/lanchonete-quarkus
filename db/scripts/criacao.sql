c4d4b17e8ce9e4d6a9726e84b820b9ece5822f10
AneDuarte <anart.lodes73@gmail.com>
2023-10-03 11:49:38 -0300

    correção script de criação de tabelas

--- db/scripts/criacao.sql
+++ db/scripts/criacao.sql
@@ -1,29 +1,29 @@
 CREATE TABLE "sabor" (
-    id int not null primary key,
+    id serial not null primary key,
     nome varchar(100) not null
 );
 
 CREATE TABLE "telefone" (
-    id int not null primary key,
+    id serial not null primary key,
     ddd varchar(3) not null,
     numero varchar(9) not null
 );
 
 CREATE TABLE "endereco" (
-    id int not null primary key,
+    id serial not null primary key,
     rua varchar(250),
     bairro varchar(100) not null,
     numero int
 );
 
 CREATE TABLE "lanche" (
-    id int not null primary key,
+    id serial not null primary key,
     nome varchar(100) not null,
     id_sabor int not null references sabor(id)
 );
 
 CREATE TABLE "usuario" (
-    id int not null primary key,
+    id serial not null primary key,
     nome varchar(250) not null,
     email varchar(100) not null,
     id_telefone int not null references telefone(id),
@@ -31,7 +31,7 @@ CREATE TABLE "usuario" (
 );
 
 CREATE TABLE "pedido" (
-    id int not null primary key,
+    id serial not null primary key,
     id_usuario int not null references usuario(id),
     id_lanche int not null references lanche(id)
 );
